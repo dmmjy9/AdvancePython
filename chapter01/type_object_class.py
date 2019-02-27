a4d9ea5e0a287399676132bab05b66cf77381085
穆俊尧 <mujunyao@mujunyaos-MacBook-Pro.local>
2019-02-27 08:43:23 +0800

    chap1 finish

---
+++ chapter01/type_object_class.py
@@ -0,0 +1,23 @@
+a = 1
+b = "abc"
+print(type(1))
+print(type(int))
+print(type(b))
+print(type(str))
+# type -> int -> 1
+# type -> class -> obj
+
+class Student:
+    pass
+
+class MyStudent(Student):
+    pass
+stu = Student()
+print(type(stu))
+print(type(Student))
+print(int.__bases__)
+print(str.__bases__)
+print(Student.__bases__)
+print(MyStudent.__bases__)
+print(type(object))
+print(object.__bases__)
